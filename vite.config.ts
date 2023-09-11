d840dddfd74cd48927626ccfae51c57482050b2b
Vlad <rotariuvladimir@gmail.com>
2023-09-11 14:51:16 +0200

    Create test for Button component to check whether it shows a button with the required test

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "frontend/src/setupTests.ts",
+    include: ["frontend/src/**/*.test.{ts,tsx}"],
+    coverage: {
+      provider: "v8",
+      reporter: ["lcov", "text"],
+      include: ["frontend/src/components/Button/*.{ts,tsx}"],
+      exclude: [
+        "**/*.test.{ts,tsx}",
+        "**/types.ts",
+        "**/*.d.ts",
+        "**/src/main.tsx",
+      ],
+    },
+  },
+});
