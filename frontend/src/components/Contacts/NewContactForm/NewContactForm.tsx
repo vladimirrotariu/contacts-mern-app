c3fa3c1af6f2dddd2db226eeba085a1ba6d5b6a8
Vlad <rotariuvladimir@gmail.com>
2023-09-11 22:57:24 +0200

    Create NewContactForm React component

---
+++ frontend/src/components/Contacts/NewContactForm/NewContactForm.tsx
@@ -0,0 +1,80 @@
+import { FormEventHandler, useState } from "react";
+
+import React from "react";
+import { NewContactFormProps } from "../../../utils/types";
+import Button from "../../Button/Button";
+import Input from "../../Input/Input";
+import "./NewContactForm.css";
+
+const NewContactForm = (props: NewContactFormProps) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPhone, setEnteredPhone] = useState("");
+
+  const nameChangeHandler = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    setEnteredName(event.currentTarget.value);
+  };
+
+  const ageChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredAge(event.currentTarget.value);
+  };
+
+  const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredEmail(event.currentTarget.value)
+  }
+
+  const phoneChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredPhone(event.currentTarget.value)
+  }
+
+  const submitContactHandler: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    props.onAddContact(enteredName, enteredAge, enteredEmail, enteredPhone);
+  };
+
+  return (
+    <section className="new-contact">
+      <h2>Add a New Contact</h2>
+      <form onSubmit={submitContactHandler}>
+        <Input
+          type="text"
+          label="Name"
+          id="name"
+          value={enteredName}
+          onChange={nameChangeHandler}
+          placeholder="Camille"
+        />
+        <Input
+          type="number"
+          label="Age"
+          id="age"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+          placeholder="19"
+        />
+        <Input
+          type="text"
+          label="Email"
+          id="email"
+          value={enteredEmail}
+          onChange={emailChangeHandler}
+          placeholder="camilledegrasse@zmail.com"
+        />
+        <Input
+          type="text"
+          label="Phone"
+          id="phone"
+          value={enteredPhone}
+          onChange={phoneChangeHandler}
+          placeholder="+31094887472999"
+        />
+        <Button type="submit" textButton={props.textButton}></Button>
+      </form>
+    </section>
+  );
+};
+
+export default NewContactForm;
