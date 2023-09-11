45f41461668fd3946c47f86202f7ca4f0ef63747
Vlad <rotariuvladimir@gmail.com>
2023-09-11 16:08:58 +0200

    Create Header React component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = (): React.ReactElement => {
+  return (
+    <header className="header">
+      <h1>My Contacts List</h1>
+    </header>
+  );
+};
+
+export default Header;
