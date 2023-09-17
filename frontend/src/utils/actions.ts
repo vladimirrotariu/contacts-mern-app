42c1fef23aca3840e5f5066c1d3f6234e0ead8a2
Vlad <rotariuvladimir@gmail.com>
2023-09-17 09:51:39 +0200

    Use phone and name to identify to be deleted contact, and fix bugs insuing from it

--- frontend/src/utils/actions.ts
+++ frontend/src/utils/actions.ts
@@ -1,3 +1,4 @@
-export const deleteContact = async (id: string) => {
-  await fetch("https://contacts-09i3.onrender.com/contact-delete", {method: "DELETE", body: id});
+export const deleteContact = async (name: string, phone: string) => {
+  const bodyData = JSON.stringify({name, phone});
+  await fetch("https://contacts-09i3.onrender.com/contact-delete", {method: "DELETE", body: bodyData});
 }
