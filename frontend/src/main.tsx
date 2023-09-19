886b254ef2146d9b1b62cc59b7ebf7cae3ddf181
Vlad <rotariuvladimir@gmail.com>
2023-09-19 18:39:23 +0200

    Add alert to warn the users that it takes like 3 mins for Render backend server warmup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App.js";
 
+alert("PLEASE wait like 3 minutes for the Contacts app to work properly, Render server for backend needs to warm up :)");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
